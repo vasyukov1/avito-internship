6b1ec5a9dd97814b3e81acf15d110bc57296ecc2
Alexander Vasyukov <vasyukov2005al@yandex.ru>
2025-11-22 17:25:14 +0300

    fix: errors

--- internal/delivery/handler.go
+++ internal/delivery/handler.go
@@ -1,7 +1,11 @@
 package delivery
 
 import (
+	"avito-internship/internal/domain"
 	"avito-internship/internal/infrastructure"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Handler struct {
@@ -10,4 +14,23 @@ type Handler struct {
 
 func NewHandler(service *infrastructure.Server) *Handler {
 	return &Handler{service}
+}
+
+func (h *Handler) handleError(c *gin.Context, err error) {
+	switch {
+	case errors.Is(err, domain.ErrTeamExists):
+		c.JSON(http.StatusBadRequest, gin.H{"error": gin.H{"code": "TEAM_EXISTS", "message": "team_name already exists"}})
+	case errors.Is(err, domain.ErrNotFound):
+		c.JSON(http.StatusNotFound, gin.H{"error": gin.H{"code": "NOT_FOUND", "message": "resource not found"}})
+	case errors.Is(err, domain.ErrPRExists):
+		c.JSON(http.StatusConflict, gin.H{"error": gin.H{"code": "PR_EXISTS", "message": "PR id already exists"}})
+	case errors.Is(err, domain.ErrPRMerged):
+		c.JSON(http.StatusConflict, gin.H{"error": gin.H{"code": "PR_MERGED", "message": "cannot reassign on merged PR"}})
+	case errors.Is(err, domain.ErrNotAssigned):
+		c.JSON(http.StatusConflict, gin.H{"error": gin.H{"code": "NOT_ASSIGNED", "message": "reviewer is not assigned to this PR"}})
+	case errors.Is(err, domain.ErrNoCandidate):
+		c.JSON(http.StatusConflict, gin.H{"error": gin.H{"code": "NO_CANDIDATE", "message": "no active replacement candidate in team"}})
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{"error": gin.H{"code": "INTERNAL_ERROR", "message": err.Error()}})
+	}
 }
